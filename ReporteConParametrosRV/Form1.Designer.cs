39b8531735514c36a3ead5abd1354c4a60fd135f
Adrian Garcia <adrian_gar016@hotmail.es>
2017-09-10 00:03:19 -0500

    Subida del proyecto report viewer
    Este proyecto de ejemplo muestra de forma basica el uso del report viewer y como se puede diseñar un report de salida con el uso de las herramientas de vs y que se unen a sql y la base de datos northwind para esta tarea

---
+++ ReporteConParametrosRV/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ReporteConParametrosEF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.ProductsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.NorthwindDataSet = new ReporteConParametrosEF.NorthwindDataSet();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.btnVerTodos = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txbBuscar = new System.Windows.Forms.TextBox();
+            this.ProductsTableAdapter = new ReporteConParametrosEF.NorthwindDataSetTableAdapters.ProductsTableAdapter();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NorthwindDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ProductsBindingSource
+            // 
+            this.ProductsBindingSource.DataMember = "Products";
+            this.ProductsBindingSource.DataSource = this.NorthwindDataSet;
+            // 
+            // NorthwindDataSet
+            // 
+            this.NorthwindDataSet.DataSetName = "NorthwindDataSet";
+            this.NorthwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.AutoSize = true;
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.ProductsBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "ReporteConParametrosEF.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 24);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Padding = new System.Windows.Forms.Padding(10);
+            this.reportViewer1.Size = new System.Drawing.Size(792, 549);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // btnVerTodos
+            // 
+            this.btnVerTodos.Location = new System.Drawing.Point(372, 0);
+            this.btnVerTodos.Name = "btnVerTodos";
+            this.btnVerTodos.Size = new System.Drawing.Size(75, 23);
+            this.btnVerTodos.TabIndex = 3;
+            this.btnVerTodos.Text = "Ver todos";
+            this.btnVerTodos.UseVisualStyleBackColor = true;
+            this.btnVerTodos.Click += new System.EventHandler(this.btnVerTodos_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(291, 0);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txbBuscar
+            // 
+            this.txbBuscar.Location = new System.Drawing.Point(12, -2);
+            this.txbBuscar.Name = "txbBuscar";
+            this.txbBuscar.Size = new System.Drawing.Size(273, 20);
+            this.txbBuscar.TabIndex = 1;
+            // 
+            // ProductsTableAdapter
+            // 
+            this.ProductsTableAdapter.ClearBeforeFill = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(792, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.btnVerTodos);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.txbBuscar);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NorthwindDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.Button btnVerTodos;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txbBuscar;
+        private System.Windows.Forms.BindingSource ProductsBindingSource;
+        private NorthwindDataSet NorthwindDataSet;
+        private NorthwindDataSetTableAdapters.ProductsTableAdapter ProductsTableAdapter;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ ReporteConParametrosRV/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReporteConParametrosEF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'NorthwindDataSet.Products' table. You can move, or remove it, as needed.
+            this.ProductsTableAdapter.mostrarTodos(this.NorthwindDataSet.Products);
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string productName = txbBuscar.Text;
+            if(productName.Length > 0)
+            {
+                this.ProductsTableAdapter.filtroPorProducto(this.NorthwindDataSet.Products,productName); //el parametro lo antecede una coma
+                this.reportViewer1.RefreshReport();
+                txbBuscar.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Debes de escribir un producto a buscar");
+            }
+        }
+
+        private void btnVerTodos_Click(object sender, EventArgs e)
+        {
+            this.ProductsTableAdapter.mostrarTodos(this.NorthwindDataSet.Products);
+
+            this.reportViewer1.RefreshReport();
+        }
+
+    }
+}
